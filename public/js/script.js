c5946e6217b07b8fdfca9b5201b6e09d9610dcc2
logesh_07_05 <naru8sasu9344@gmail.com>
2025-04-20 18:57:25 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -31,9 +31,10 @@ if (navigator.geolocation) {
                             iconUrl: "https://maps.google.com/mapfiles/ms/icons/green-dot.png",
                             iconSize: [32, 32], // Adjust size if needed
                         })
-                    );          
+                    );  
+                    sendLocationToPython(location.name, location.latitude, location.longitude, status, location.esp32_id);
                     if(area !== location.name){
-                        sendLocationToPython(location.name, location.latitude, location.longitude, status, location.esp32_id);
+                        
                         localStorage.setItem("area", location.name);
                         area=location.name;
                     }
@@ -46,8 +47,9 @@ if (navigator.geolocation) {
                             iconSize: [32, 32], // Adjust size if needed
                         })
                     );
+                    sendLocationToPython(location.name, location.latitude, location.longitude, status, location.esp32_id);
                     if(area === location.name){
-                        sendLocationToPython(location.name, location.latitude, location.longitude, status, location.esp32_id);
+                        
                         localStorage.setItem("area", "None");
                         area="None";
                     }
