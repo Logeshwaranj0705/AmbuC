19646c0896a2f55e89d082476b98c62c5401e75f
logesh_07_05 <naru8sasu9344@gmail.com>
2025-04-20 19:07:56 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -31,10 +31,9 @@ if (navigator.geolocation) {
                             iconUrl: "https://maps.google.com/mapfiles/ms/icons/green-dot.png",
                             iconSize: [32, 32], // Adjust size if needed
                         })
-                    );  
-                    sendLocationToPython(location.name, location.latitude, location.longitude, status, location.esp32_id);
+                    );                      
                     if(area !== location.name){
-                        
+                        sendLocationToPython(location.name, location.latitude, location.longitude, status, location.esp32_id);
                         localStorage.setItem("area", location.name);
                         area=location.name;
                     }
@@ -46,10 +45,9 @@ if (navigator.geolocation) {
                             iconUrl: "https://maps.google.com/mapfiles/ms/icons/red-dot.png",
                             iconSize: [32, 32], // Adjust size if needed
                         })
-                    );
-                    sendLocationToPython(location.name, location.latitude, location.longitude, status, location.esp32_id);
+                    );                    
                     if(area === location.name){
-                        
+                        sendLocationToPython(location.name, location.latitude, location.longitude, status, location.esp32_id);
                         localStorage.setItem("area", "None");
                         area="None";
                     }
